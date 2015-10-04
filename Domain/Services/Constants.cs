c7a62118405b289f63fa3319d40f994450ab7fe4
Nikita Bozhkov <pitlis95@gmail.com>
2015-10-04 20:37:32 +0300

    Константы - для борьбы с магическими числами.

---
+++ Domain/Services/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public static class Constants
+    {
+        public const int WEEKS_IN_SCHEDULE = 2;
+        public const int DAYS_IN_WEEK = 6;
+        public const int CLASSES_IN_DAY = 6;
+    }
+}
