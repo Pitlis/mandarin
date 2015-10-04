7a2618fa94aa52f7ea98e5ec3090e48cb50b09ed
Nikita Bozhkov <pitlis95@gmail.com>
2015-10-04 21:18:37 +0300

    Класс пары. Одна лаба у одного препода у одной подгруппы - один объект. Одна лекция у одного препода у трех подгрупп - один объект.

--- Domain/Model/StudentsClass.cs
+++ Domain/Model/StudentsClass.cs
@@ -9,5 +9,22 @@ namespace Domain.Model
 {
     public class StudentsClass
     {
+        public StudentSubGroup[] SubGroups { get; private set; }
+        public Teacher[] Teacher { get; private set; }
+        //Несколько преподавателей - например для пары Лаба по физике
+        //В такую пару будут входить 2 подгруппы и 2 препода
+        //Пара проходит в одно время в одной аудитории
+
+        public string Name { get; private set; }
+        public ClassRoomType[] RequireForClassRoom { get; private set; }
+
+
+        public StudentsClass(IEnumerable<StudentSubGroup> subGroups, IEnumerable<Teacher> teacher, string name, IEnumerable<ClassRoomType> requireForClassRoom)
+        {
+            SubGroups = subGroups.ToArray<StudentSubGroup>();
+            Teacher = teacher.ToArray<Teacher>();
+            Name = name;
+            RequireForClassRoom = requireForClassRoom.ToArray<ClassRoomType>();
+        }
     }
 }
