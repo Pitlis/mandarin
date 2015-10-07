7b154eaec86e173c2d14c43bcc9bfdd13730277e
Nikita Bozhkov <pitlis95@gmail.com>
2015-10-07 18:44:03 +0300

    Поиск шести пар подряд.

---
+++ OtherFactors/SixStudentsClasses.cs
@@ -0,0 +1,72 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Services;
+using Domain.Model;
+
+namespace OtherFactors
+{
+    public class SixStudentsClasses : IFactor
+    {
+        int fine;
+        bool isBlock;
+
+        public int GetFineOfAddedClass(ISchedule schedule, EntityStorage eStorage)
+        {
+            StudentSubGroup[] groups = schedule.GetTempClass().SubGroups;
+            int fineResult = 0;
+            for (int groupIndex = 0; groupIndex < groups.Length; groupIndex++)
+            {
+                PartialSchedule groupSchedule = schedule.GetPartialSchedule(groups[groupIndex]);
+                for (int dayIndex = 0; dayIndex < Constants.WEEKS_IN_SCHEDULE*Constants.DAYS_IN_WEEK; dayIndex++)
+                {
+                    if (Array.FindAll<StudentsClass>(groupSchedule.GetClassesOfDay(dayIndex), (c) => c != null).Count() == 6)
+                    {
+                        fineResult += fine;
+                    }
+                }
+            }
+            return fineResult;
+        }
+
+        public int GetFineOfFullSchedule(ISchedule schedule, EntityStorage eStorage)
+        {
+            int fineResult = 0;
+            for (int groupIndex = 0; groupIndex < eStorage.StudentSubGroups.Length; groupIndex++)
+            {
+                PartialSchedule groupSchedule = schedule.GetPartialSchedule(eStorage.StudentSubGroups[groupIndex]);
+                for (int dayIndex = 0; dayIndex < Constants.WEEKS_IN_SCHEDULE * Constants.DAYS_IN_WEEK; dayIndex++)
+                {
+                    if (Array.FindAll<StudentsClass>(groupSchedule.GetClassesOfDay(dayIndex), (c) => c != null).Count() == 6)
+                    {
+                        fineResult += fine;
+                    }
+                }
+            }
+            return fineResult;
+        }
+
+        public string GetName()
+        {
+            return "6 пар";
+        }
+        public string GetDescription()
+        {
+            return "Шесть пар в день";
+        }
+
+        public void Initialize(int fine = 0, bool isBlock = false)
+        {
+            if (fine >= 0 && fine <= 100)
+            {
+                this.fine = fine;
+                this.isBlock = isBlock;
+                if (fine == 100)
+                    this.isBlock = true;
+            }
+        }
+    }
+}
