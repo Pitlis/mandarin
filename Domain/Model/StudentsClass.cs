3d07a3b62346d38cb784fd3d845be690639e56de
Nikita Bozhkov <pitlis95@gmail.com>
2015-11-10 22:59:59 +0300

    ---

--- Domain/Model/StudentsClass.cs
+++ Domain/Model/StudentsClass.cs
@@ -27,5 +27,61 @@ namespace Domain.Model
             Name = name;
             RequireForClassRoom = requireForClassRoom.ToArray<ClassRoomType>();
         }
+
+        public static bool StudentClassEquals(StudentsClass c1, StudentsClass c2)
+        {
+            if (c1.Name == c2.Name)
+            {
+                foreach (Teacher teacher in c1.Teacher)
+                {
+                    if (!c2.Teacher.Contains(teacher))
+                    {
+                        return false;
+                    }
+                }
+                foreach (Teacher teacher in c2.Teacher)
+                {
+                    if (!c1.Teacher.Contains(teacher))
+                    {
+                        return false;
+                    }
+                }
+                foreach (StudentSubGroup group in c1.SubGroups)
+                {
+                    if (!c2.SubGroups.Contains(group))
+                    {
+                        return false;
+                    }
+                }
+                foreach (StudentSubGroup group in c2.SubGroups)
+                {
+                    if (!c1.SubGroups.Contains(group))
+                    {
+                        return false;
+                    }
+                }
+
+                foreach (ClassRoomType roomType in c1.RequireForClassRoom)
+                {
+                    if (!c2.RequireForClassRoom.Contains(roomType))
+                    {
+                        return false;
+                    }
+                }
+
+                foreach (ClassRoomType roomType in c2.RequireForClassRoom)
+                {
+                    if (!c1.RequireForClassRoom.Contains(roomType))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
