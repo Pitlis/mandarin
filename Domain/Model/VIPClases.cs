f2478a69fe8ad04533e3900ad681f6f0782a2ed1
jonny1sniper <nevidimkapunk@gmail.com>
2015-12-02 22:41:52 +0300

    Класс для VIP пар

---
+++ Domain/Model/VIPClases.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class VIPClases
+    {
+        public StudentsClass Cla { get; set; }
+        public int Time { get; set; }
+        public ClassRoom Aud { get; set; }
+        public VIPClases(StudentsClass cla, int time, ClassRoom aud)
+        {
+            this.Cla = cla;
+            this.Time = time;
+            this.Aud = aud;
+        }
+    }
+}
--- ESProject/Code/Setting.cs
+++ ESProject/Code/Setting.cs
@@ -123,18 +123,4 @@ namespace Presentation.Code
             this.Aud = aud;
         }
     }
-
-    class VIPClases
-    {
-        public StudentsClass Cla { get; set; }
-        public int Time { get; set; }
-        public ClassRoom Aud { get; set; }
-        public VIPClases(StudentsClass cla, int time, ClassRoom aud)
-        {
-            this.Cla = cla;
-            this.Time = time;
-            this.Aud = aud;
-        }
-
-    }
 }
