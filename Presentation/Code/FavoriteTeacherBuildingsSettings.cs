6a1eea7beff7bf7c0e8b61e85c9479163ee2e6b9
jonny1sniper <nevidimkapunk@gmail.com>
2016-01-19 18:04:16 +0300

    Класс предпочтительных корпусов преподавателей

---
+++ Presentation/Code/FavoriteTeacherBuildingsSettings.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Domain.Model;
+using System.Linq;
+
+namespace Presentation.Code
+{
+    class FavoriteTeacherBuildingsSettings
+    {
+        public Dictionary<Teacher, List<int>> favTeachersBuildings { get; set; }
+
+        public FavoriteTeacherBuildingsSettings()
+        {
+            int factorIndex = CurrentBase.Factors.FindIndex(c => c.FactorName == "FavoriteTeachersBuildings");
+            favTeachersBuildings = (Dictionary<Teacher, List<int>>)CurrentBase.Factors[factorIndex].Data;
+            if (favTeachersBuildings == null)
+                favTeachersBuildings = new Dictionary<Teacher, List<int>>();
+        }
+
+        public void AddFavoriteBuilding(Teacher teacher, int building)
+        {
+            if (favTeachersBuildings.ContainsKey(teacher))
+            {
+                favTeachersBuildings[teacher].Add(building);
+            }
+            else
+            {
+                favTeachersBuildings.Add(teacher, new List<int> { building });
+            }
+        }
+
+        public void RemoveFavoriteBuilding(Teacher teacher, int building)
+        {
+            if (favTeachersBuildings.ContainsKey(teacher))
+            {
+                favTeachersBuildings[teacher].Remove(building);
+            }
+        }
+
+        public List<int> GetUnfavoriteBuildings(Teacher teacher)
+        {
+            List<int> unfavBuildings = new List<int>();
+            if (favTeachersBuildings.ContainsKey(teacher))
+            {
+                foreach (int building in CurrentBase.EStorage.ClassRooms.Select(c => c.Housing).Distinct())
+                {
+                    if (favTeachersBuildings[teacher].IndexOf(building) == -1)
+                    {
+                        unfavBuildings.Add(building);
+                    }
+                }
+            }
+            return unfavBuildings;
+        }
+    }
+}
