ccd140b20f7194af64a3578305ef6f108a82b74d
jonny1sniper <nevidimkapunk@gmail.com>
2016-01-22 21:21:48 +0300

    bug fix - выгрузка базы данных

--- Presentation/Code/Logic.cs
+++ Presentation/Code/Logic.cs
@@ -28,17 +28,11 @@ namespace Presentation.Code
         //TODO Заглушка для Dependency Inversion
         public void DI()
         {
-            //Repo = new Data.DataRepository();
-            //Repo.Init(new string[] { @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\СЕРГЕЙ\DOCUMENTS\ESPROJECT\ESPROJECT\BIN\DEBUG\BD4.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" });
-
-            Repo = new MockDataBase.MockRepository();
-            Repo.Init(null);
             Factors = new List<FactorSettings>();
 
             AllocConsole();
             loggingService = new NLogLoggingService();
 
-            //storage = DataConvertor.ConvertData(Repo.GetTeachers(), Repo.GetStudentsGroups(), Repo.GetClassRoomsTypes(), Repo.GetClassRooms(), Repo.GetStudentsClasses());
             storage = CurrentBase.EStorage;
 
             vip = new Setting(storage, storage.Classes);
--- Presentation/Code/Setting.cs
+++ Presentation/Code/Setting.cs
@@ -23,12 +23,6 @@ namespace Presentation.Code
         public Setting()
         {
             LVIP = new List<FixedClasses>();
-            Repo = new MockDataBase.MockRepository();
-            Repo.Init(null);
-            //Repo = new Data.DataRepository();
-            //Repo.Init(new string[] { @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\СЕРГЕЙ\DOCUMENTS\ESPROJECT\ESPROJECT\BIN\DEBUG\BD4.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" });
-
-            //storage = DataConvertor.ConvertData(Repo.GetTeachers(), Repo.GetStudentsGroups(), Repo.GetClassRoomsTypes(), Repo.GetClassRooms(), Repo.GetStudentsClasses());
             storage = CurrentBase.EStorage;
             this.Clases = storage.Classes;
             LVIPB = new List<VIPClasesBin>();
