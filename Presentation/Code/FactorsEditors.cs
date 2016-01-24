b43fc83d1845ec06521959b5008914486f16bb46
Nikita Bozhkov <pitlis95@gmail.com>
2016-01-24 20:21:48 +0300

    Форма и класс для настройки анализаторов.

---
+++ Presentation/Code/FactorsEditors.cs
@@ -0,0 +1,78 @@
+﻿using Domain.FactorInterfaces;
+using Domain.Services;
+using Presentation.FactorsDataEditors;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Code
+{
+    static class FactorsEditors
+    {
+        static Dictionary<Guid, Type> factorEditors;
+
+        public static void InitFactorEditor(FactorSettings factorSettings, IFactorEditor editor)
+        {
+            IFactor factor = factorSettings.CreateInstance();
+
+            editor.Init(
+                factor.GetName(),
+                factor.GetDescription(),
+                ((IFactorFormData)factor).GetUserInstructions(),
+                ((IFactorFormData)factor).FilterStorage(GetDeepCopy()),
+                factorSettings);
+        }
+        public static void BeforeCloseFactorEditor(FactorSettings factorSettings)
+        {
+             RestoreFactorsData(factorSettings);
+        }
+
+        public static void RestoreFactorsData(FactorSettings factorSettings)
+        {
+            if (factorSettings.DataTypeGuid.Value.Equals(Guid.Parse("6CF3F58B5FA2464B8FF55B8E1724E0C9")))
+            {
+
+                return;
+            }
+        }
+
+
+        //список поддерживаемых системой редакторов данных для анализаторов
+        public static Dictionary<Guid, Type> GetFactorEditors()
+        {
+            if (factorEditors == null)
+            {
+                factorEditors = new Dictionary<Guid, Type>();
+                factorEditors.Add(new Guid("6CF3F58B-5FA2-464B-8FF5-5B8E1724E0C9"), typeof(FavoriteTeacherBuildingForm));
+
+
+
+            }
+            return factorEditors;
+        }
+
+        //не надо смотреть на это.... 
+        //:o
+        public static EntityStorage GetDeepCopy()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            EntityStorage copy = null;
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                formatter.Serialize(memStream, CurrentBase.EStorage);
+                memStream.Flush();
+                memStream.Position = 0;
+                copy = (EntityStorage)formatter.Deserialize(memStream);
+            };
+            return copy;
+        }
+
+        #region Восстановление ссылок данных анализаторов
+
+        #endregion
+    }
+}
---
+++ Presentation/FactorSettingsForm.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Domain;
+using Domain.Services;
+using Domain.Model;
+using System.Reflection;
+using System.IO;
+using System.Xml.Serialization;
+using MandarinCore;
+using Domain.FactorInterfaces;
+using Presentation.Code;
+using Presentation.FactorsDataEditors;
+
+namespace Presentation
+{
+    /// <summary>
+    /// Логика взаимодействия для FactorSettingsForm.xaml
+    /// </summary>
+    public partial class FactorSettingsForm : Window
+    {
+        List<FactorSettingRecord> FactorRecords;
+
+        public FactorSettingsForm()
+        {
+            InitializeComponent();
+            FactorRecords = LoadFactorSettingRecords().ToList();
+            this.DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = FactorRecords;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFactorSettings();
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите завершить настройку?\n" +
+                "Все несохраненные изменения будут потеряны!", "Mandarin",
+                MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите сбросить все настройки анализаторов?\n" +
+                "Все введенные данные (например предпочтения преподавателей по корпусам) будут потеряны!", "Mandarin",
+                MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                FactorsLoader.SetDefaultSettings();
+                FactorRecords = LoadFactorSettingRecords().ToList();
+                dataGrid.ItemsSource = null;
+                dataGrid.ItemsSource = FactorRecords;
+            }
+        }
+
+        private void btnEditData_Click(object sender, RoutedEventArgs e)
+        {
+            FactorSettingRecord row = ((FrameworkElement)sender).DataContext as FactorSettingRecord;
+            OpenFactorEditor(row);
+        }
+
+        #region Code
+
+        IEnumerable<FactorSettingRecord> LoadFactorSettingRecords()
+        {
+            List<FactorSettingRecord> factorRecords = new List<FactorSettingRecord>();
+            foreach (FactorSettings factorSettings in CurrentBase.Factors)
+            {
+                IFactor factorInstance = factorSettings.CreateInstance();
+                FactorSettingRecord factorRecord = new FactorSettingRecord();
+
+                factorRecord.ReferenceToSettings = factorSettings;
+                factorRecord.Name = factorInstance.GetName();
+                factorRecord.Description = factorInstance.GetDescription();
+                factorRecord.Fine = factorSettings.Fine;
+
+                factorRecord.DataType = factorSettings.DataTypeGuid;
+                factorRecord.Data = factorSettings.Data;
+
+                if (factorRecord.DataType.HasValue)
+                {
+                    factorRecord.UseProgramData = factorInstance is IFactorProgramData;
+                    factorRecord.UseUsersData = factorInstance is IFactorFormData && FactorsEditors.GetFactorEditors().ContainsKey(factorRecord.DataType.Value);
+                }
+
+                factorRecords.Add(factorRecord);
+            }
+
+            return factorRecords;
+        }
+        void SaveFactorSettings()
+        {
+            foreach (FactorSettingRecord record in FactorRecords)
+            {
+                FactorSettings factor = record.ReferenceToSettings;
+
+                factor.Fine = record.Fine;
+                factor.Data = record.Data;
+            }
+        }
+        void OpenFactorEditor(FactorSettingRecord factorRecord)
+        {
+            try
+            {
+                Type editorType = FactorsEditors.GetFactorEditors()[factorRecord.DataType.Value];
+                Window editorForm = (Window)Activator.CreateInstance(editorType);
+                
+                FactorsEditors.InitFactorEditor(factorRecord.ReferenceToSettings, (IFactorEditor)editorForm);
+                editorForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+    }
+
+    class FactorSettingRecord
+    {
+        public FactorSettings ReferenceToSettings { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        int fine;
+        public int Fine
+        {
+            get { return fine; }
+            set
+            {
+                if (value > 100 || value < 0)
+                    MessageBox.Show("Значение должно находиться в диапазоне от 0 до 100");
+                else { fine = value; }
+            }
+        }
+
+        public bool UseProgramData { get; set; }
+        public bool UseUsersData { get; set; }
+
+        public Guid? DataType { get; set; }
+        public object Data { get; set; }
+    }
+}
