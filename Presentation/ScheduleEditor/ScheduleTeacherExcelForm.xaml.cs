8e6d3c89f369d77c77de950aefc6eeef8d75244b
Ser95 <puss95@yandex.by>
2016-01-24 20:33:07 +0300

    Добавлена форма для выгрузки расписания отдельных преподавателей в Excel, без логики

---
+++ Presentation/ScheduleEditor/ScheduleTeacherExcelForm.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Domain.Services;
+namespace Presentation.ScheduleEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для ScheduleTeacherExcel.xaml
+    /// </summary>
+    public partial class ScheduleTeacherExcel : Window
+    {
+        FullSchedule fullshedule;
+        public ScheduleTeacherExcel(FullSchedule fullshedule)
+        {
+            InitializeComponent();
+            this.fullshedule = fullshedule;
+        }
+
+        void FillingScrData()
+        {
+            
+        }
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if()
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
