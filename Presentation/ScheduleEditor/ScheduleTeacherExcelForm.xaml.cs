3dd87de3cda0a40e53f60d5cd541fbdefa79245b
Ser95 <puss95@yandex.by>
2016-01-25 16:10:34 +0300

    Дополнена форма для выгрузки расписания преподавателей в Excel

--- Presentation/ScheduleEditor/ScheduleTeacherExcelForm.xaml.cs
+++ Presentation/ScheduleEditor/ScheduleTeacherExcelForm.xaml.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Domain.Services;
+using Domain.Model;
+using Domain.DataFiles;
+using Domain;
+using Microsoft.Win32;
+using System.Threading;
+using Presentation.Controls;
+using MaterialDesignThemes.Wpf;
+
 namespace Presentation.ScheduleEditor
 {
     /// <summary>
@@ -19,30 +18,206 @@ namespace Presentation.ScheduleEditor
     /// </summary>
     public partial class ScheduleTeacherExcel : Window
     {
-        FullSchedule fullshedule;
-        public ScheduleTeacherExcel(FullSchedule fullshedule)
+        Schedule schedule;
+        CheckBox[] chTeacher;
+        Thread thread;
+        string filepath;
+        Teacher[] checkedteachers;
+        public ScheduleTeacherExcel(Schedule schedule)
         {
             InitializeComponent();
-            this.fullshedule = fullshedule;
+            this.schedule = schedule;
+        }
+        public ScheduleTeacherExcel(string filepath)
+        {
+            InitializeComponent();
+            this.schedule = Code.ScheduleLoader.LoadSchedule(filepath);
+            FillingScrData();
         }
 
         void FillingScrData()
         {
-            
+            List<Teacher> teachers = schedule.EStorage.Teachers.ToList();
+            chTeacher = new CheckBox[teachers.Count];
+            Grid grid = new Grid();
+            for (int indexchb = 0; indexchb < teachers.Count; indexchb++)
+            {
+                chTeacher[indexchb] = new CheckBox()
+                {
+                    Content = teachers[indexchb].Name,
+                    Margin = new Thickness(0, indexchb * 20, 0, 0),
+                };
+                chTeacher[indexchb].Checked += checkBox_Checked;
+                chTeacher[indexchb].Unchecked += checkBox_Unchecked;
+                grid.Children.Add(chTeacher[indexchb]);
+              
+            }
+            scrData.Content = grid;
         }
+        
         private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //if()
+            Search();           
         }
-
+        void Search()
+        {
+            string search = tbSearch.Text.ToUpper();
+            int j = 0;
+            Grid grid = new Grid();
+            scrData.Content = null;
+            for (int i = 0; i < chTeacher.Count(); i++)
+            {
+                if (!chTeacher[i].Content.ToString().ToUpper().Contains(search))
+                {
+                    chTeacher[i].Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    chTeacher[i].Visibility = Visibility.Visible;
+                    chTeacher[i].Margin = new Thickness(0, 15 + 20 * j, 0, 0);
+                    if (chTeacher[i].Parent != null)
+                    {
+                        var parent = (Panel)chTeacher[i].Parent;
+                        parent.Children.Remove(chTeacher[i]);
+                    }
+                    grid.Children.Add(chTeacher[i]);
+                    j++;
+                }
+            }
+            scrData.Content = grid;
+        }
         private void checkBox_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            CountChecked();
         }
 
         private void checkBox_Checked(object sender, RoutedEventArgs e)
         {
+            CountChecked();
+        }
+        void CountChecked()
+        {           
+            int count = 0;
+            string nameteacher="";
+            foreach(var item in chTeacher)
+            {
+                if (item.IsChecked == true)
+                {
+                    if(count==0) nameteacher =item.Content.ToString();
+                    else nameteacher += "," + item.Content.ToString();
+                    count++;                   
+                }
+            }
+            if (count == 0) btnSave.IsEnabled = false;
+            else btnSave.IsEnabled = true;
+            lblCount.ToolTip = nameteacher;
+            lblprepod.ToolTip = nameteacher;
+            lblCount.Content = count.ToString();
+            if (thread != null && thread.ThreadState == ThreadState.Running)
+            {
+                btnSave.IsEnabled = false;
+                return;
+            }
+            else btnSave.IsEnabled = true;
+        }
 
+        private void btnClean_Click(object sender, RoutedEventArgs e)
+        {
+            
+            tbSearch.Text = "";
+            foreach(var item in chTeacher)
+            {
+                item.IsChecked = false;
+            }
+        }
+
+        private  void  btnSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            tbSearch.Text = "";
+            foreach (var item in chTeacher)
+            {
+                item.IsChecked = true;
+            }
+        }
+        async void LoadToExcel()
+        {
+            int countCheck = Convert.ToInt32(lblCount.Content.ToString());
+            checkedteachers = new Teacher[countCheck];
+            int indexChecked = 0;
+            for (int indexCheckBox = 0; indexCheckBox < chTeacher.Count(); indexCheckBox++)
+            {
+                if (chTeacher[indexCheckBox].IsChecked == true)
+                {
+                    checkedteachers[indexChecked] = schedule.EStorage.Teachers[indexCheckBox];
+                    indexChecked++;
+                }
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel (*.xlsx|*.xlsx";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == true)//сделать формирование в отдельном потоке
+            {
+                filepath = saveFileDialog.FileName;
+                thread = new Thread(new ThreadStart(SaveExcel));
+                thread.Start();                
+                var infoWindow = new InfoWindow
+                {
+                    Message = { Text = "Расписание будет сформировано:\n" + filepath+"\nПожалуйста подождите" }
+                };
+                await DialogHost.Show(infoWindow, "ScheduleTeacherForExcel");
+                btnSave.IsEnabled = false;
+
+            }            
+        }
+        void SaveExcel()
+        {
+            Code.ScheduleExcelTeacher excel = new Code.ScheduleExcelTeacher(filepath, schedule, schedule.EStorage);
+            excel.LoadToExcel(checkedteachers);
+            this.Dispatcher.Invoke(new  Action(async delegate ()
+            {
+                var infoWindow = new InfoWindow
+                {
+                    Message = { Text = "Расписание сформировано:\n" + filepath }
+                };
+                await DialogHost.Show(infoWindow, "ScheduleTeacherForExcel");
+                btnSave.IsEnabled = true;
+            }));
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (thread != null && thread.ThreadState == ThreadState.Running)
+            {
+                btnSave.IsEnabled = false;
+                return;
+            }
+            if (lblCount.Content.ToString() == "0")
+            {
+                btnSave.IsEnabled = false;
+                return;
+            }
+            LoadToExcel();
+        }
+
+        bool isClose = true;
+        private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isClose)
+            {
+                e.Cancel = true;
+            }
+            var dialogWindow = new DialogWindow
+            {
+                Message = { Text = "Вы действительно хотите выйти?" }
+            };
+
+            object result = await DialogHost.Show(dialogWindow, "ScheduleTeacherForExcel");
+            if (result != null && (bool)result == true)
+            {
+                if (thread != null) thread.Abort();
+                isClose = false;
+                Close();
+            }
         }
     }
-}
+    }
