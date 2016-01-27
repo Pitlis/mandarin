2b499ea033b860be9756ba9dc71ab3e8ae2827e0
Ser95 <puss95@yandex.by>
2016-01-27 15:24:54 +0300

    Добавлена форма для редактирования хранилища

---
+++ Presentation/StorageEditor/StorageEditor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model;
+namespace Presentation.StorageEditor
+{
+     class StorageEditor
+    {
+        Domain.Services.EntityStorage eStorage;
+        public StorageEditor()
+        {
+            eStorage = Code.CurrentBase.EStorage;
+        }
+        public void Save()
+        {
+            Code.CurrentBase.EStorage = eStorage;
+            ///////////////////////////////////
+        }
+        #region ClassRoomType
+        public List<ClassRoomType> GetClassRoomType()
+        {
+            return eStorage.ClassRoomsTypes.ToList();
+        }
+        int IDTypes()
+        {
+            int ID=0;
+            if (eStorage.ClassRoomsTypes.Count() == 0) return 0;
+            ClassRoomType t = eStorage.ClassRoomsTypes.ToList().Last();
+            ID = ((Domain.IDomainIdentity<ClassRoomType>)t).ID;
+            return ID++;
+        }
+       public  void AddType(string Description)
+        {
+            //List<ClassRoomType> type = eStorage.ClassRoomsTypes.ToList();
+            //type.Add(new ClassRoomType(IDTypes(), Description));
+            //eStorage.ClassRoomsTypes=type;
+            eStorage.ClassRoomsTypes.ToList().Add(new ClassRoomType(IDTypes(), Description));
+        }
+        public void DelType(ClassRoomType type)
+        {
+          bool y=eStorage.ClassRoomsTypes.ToList().Remove(type);
+        }
+        //void EditTypes(ClassRoomType type,string Description)
+        //{
+        //    foreach(var item in eStorage.ClassRoomsTypes.ToList())
+        //    {
+        //        if(item==type)
+        //        {
+        //            item.Description = Description;
+        //        }
+        //    }
+        //}
+        public bool ExistClassRoomType(string Description)
+        {
+            foreach (var item in eStorage.ClassRoomsTypes.ToList())
+            {
+                if (item.Description == Description)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int ExistTypeInClassRoom(ClassRoomType type)
+        {          
+            int count = 0;
+            foreach(var item in eStorage.ClassRooms.ToList())
+            {
+                if (item.ClassRoomTypes == null) return 0;
+                foreach(var types in item.ClassRoomTypes.ToList())
+                {
+                    if (type == types) count++;
+                }
+            }
+            return count;
+        }
+        public int ExistTypeInClasses(ClassRoomType type)
+        {
+           
+            int count = 0;
+            foreach(var item in eStorage.Classes.ToList())
+            {
+                if (item.RequireForClassRoom == null) return 0;
+                foreach (var types in item.RequireForClassRoom.ToList())
+                {
+                    if (type == types) count++;
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Presentation/StorageEditor/StorageEditorForm.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Presentation.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Domain.Model;
+using MaterialDesignThemes.Wpf;
+using Presentation.Controls;
+
+namespace Presentation.StorageEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для StorageEditorForm.xaml
+    /// </summary>
+    public partial class StorageEditorForm : UserControl
+    {
+
+        StorageEditor storageEditor;
+        public ContentControl contentControl { get; set; }
+        public StorageEditorForm()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            storageEditor = new StorageEditor();
+            LoadTypes();
+        }
+        void LoadTypes()
+        {
+            tiTypesListView.ItemsSource = null;
+            tiTypesListView.ItemsSource = storageEditor.GetClassRoomType();
+        }
+        bool ADDTypes()
+        {
+            if (storageEditor.ExistClassRoomType(tiTypestbDescription.Text)) return false;
+            storageEditor.AddType(tiTypestbDescription.Text);
+            return true;
+        }
+
+        private async void tiTypesbtnADD_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (ADDTypes())
+            {
+                tiTypestbDescription.Text = "";
+                var infoWindow = new InfoWindow
+                {
+                    Message = { Text = "Тип Добавлен" }
+                };
+                await DialogHost.Show(infoWindow, "StorageEditorHost");
+                LoadTypes();
+            }
+            else
+            {
+                tiTypestbDescription.Text = "";
+                var infoWindow = new InfoWindow
+                {
+                    Message = { Text = "Такой тип уже существует" }
+                };
+                await DialogHost.Show(infoWindow, "StorageEditorHost");
+            }
+        }
+        async void DelType()
+        {
+            if(tiTypesListView.SelectedIndex==-1)
+            {
+                var infoWindow = new InfoWindow
+                {
+                    Message = { Text = "Необходимо выбрать тип" }
+                };
+                await DialogHost.Show(infoWindow, "StorageEditorHost");
+            }
+            string rezult = "";
+            if(storageEditor.ExistTypeInClassRoom((ClassRoomType)tiTypesListView.SelectedItem)!=0)
+            {
+                rezult = "данный тип входит в " + storageEditor.ExistTypeInClassRoom((ClassRoomType)tiTypesListView.SelectedItem).ToString() + " аудиторий\n";
+            }
+            if (storageEditor.ExistTypeInClasses((ClassRoomType)tiTypesListView.SelectedItem) != 0)
+            {
+                rezult = "данный тип входит в " + storageEditor.ExistTypeInClasses((ClassRoomType)tiTypesListView.SelectedItem).ToString() + " пар\n";
+            }
+            if(rezult!="")
+            {
+                var infoWindow = new InfoWindow
+                {
+                    Message = { Text = "Удаление невозможно: "+rezult }
+                };
+                await DialogHost.Show(infoWindow, "StorageEditorHost");
+                return;
+            }
+            DelType();
+
+
+        }
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            storageEditor.Save();
+            storageEditor = new StorageEditor();
+            var infoWindow = new InfoWindow
+            {
+                Message = { Text = "Сохранено" }
+            };
+            await DialogHost.Show(infoWindow, "StorageEditorHost");
+        }
+
+        private void tiTypesbtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            storageEditor.DelType((ClassRoomType)tiTypesListView.SelectedItem);
+            LoadTypes();
+        }
+    }
+    }
+
