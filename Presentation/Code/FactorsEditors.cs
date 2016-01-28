8c3fcbaaef6e5d31ff0407811f8b3a60fedd003d
Nikita Bozhkov <pitlis95@gmail.com>
2016-01-28 23:25:18 +0300

    Восстановление ссылок для настраиваемых пользователем анализаторов.

--- Presentation/Code/FactorsEditors.cs
+++ Presentation/Code/FactorsEditors.cs
@@ -1,4 +1,6 @@
-﻿using Domain.FactorInterfaces;
+﻿using Domain;
+using Domain.FactorInterfaces;
+using Domain.Model;
 using Domain.Services;
 using Presentation.FactorsDataEditors;
 using System;
@@ -26,16 +28,28 @@ namespace Presentation.Code
                 ((IFactorFormData)factor).FilterStorage(GetDeepCopy()),
                 factorSettings);
         }
-        public static void BeforeCloseFactorEditor(FactorSettings factorSettings)
+
+        //вызов непосредственно перез запуском формирования расписания
+        public static void RunUpFactorSettings()
         {
-             RestoreFactorsData(factorSettings);
+
         }
 
         public static void RestoreFactorsData(FactorSettings factorSettings)
         {
-            if (factorSettings.DataTypeGuid.Value.Equals(Guid.Parse("6CF3F58B5FA2464B8FF55B8E1724E0C9")))
+            if (factorSettings.DataTypeGuid.Value.Equals(Guid.Parse("6CF3F58B-5FA2-464B-8FF5-5B8E1724E0C9")))
             {
-
+                factorSettings.Data = RestoreLinks((IDictionary<Teacher, IEnumerable<int>>)factorSettings.Data, CurrentBase.EStorage);
+                return;
+            }
+            if (factorSettings.DataTypeGuid.Value.Equals(Guid.Parse("FA8861A3-02A1-4638-AF20-DF29A61A50F5")))
+            {
+                factorSettings.Data = RestoreLinks((IDictionary<Teacher, IEnumerable<ClassRoom>>)factorSettings.Data, CurrentBase.EStorage);
+                return;
+            }
+            if (factorSettings.DataTypeGuid.Value.Equals(Guid.Parse("37DCA975-0CB9-4DEC-9DAD-93CDBC0D0599")))
+            {
+                factorSettings.Data = RestoreLinks((IEnumerable<FixedClasses>)factorSettings.Data, CurrentBase.EStorage);
                 return;
             }
         }
@@ -72,7 +86,73 @@ namespace Presentation.Code
         }
 
         #region Восстановление ссылок данных анализаторов
+        public static object RestoreLinks(IDictionary<Teacher, IEnumerable<int>> data, EntityStorage storage)
+        {
+            Dictionary<Teacher, List<int>> restoredData = new Dictionary<Teacher, List<int>>();
+            foreach (var item in data)
+            {
+                Teacher teacher = item.Key;
+                List<int> buildings = item.Value.ToList();
+                restoredData.Add(storage.GetReference(teacher), buildings);
+            }
 
+            return restoredData;
+        }
+        public static object RestoreLinks(IDictionary<Teacher, IEnumerable<ClassRoom>> data, EntityStorage storage)
+        {
+            Dictionary<Teacher, List<ClassRoom>> restoredData = new Dictionary<Teacher, List<ClassRoom>>();
+            foreach (var item in data)
+            {
+                Teacher teacher = item.Key;
+                List<ClassRoom> classRooms = new List<ClassRoom>();
+                foreach (var classRoom in item.Value)
+                {
+                    classRooms.Add(storage.GetReference(classRoom));
+                }
+                restoredData.Add(storage.GetReference(teacher), classRooms);
+            }
+
+            return restoredData;
+        }
+        public static object RestoreLinks(IEnumerable<FixedClasses> data, EntityStorage storage)
+        {
+            List<FixedClasses> restoredData = new List<FixedClasses>();
+            foreach (var fixedClass in data)
+            {
+                restoredData.Add(new FixedClasses((StudentsClass)RestoreLinks(fixedClass.Room, storage), fixedClass.Time, (ClassRoom)RestoreLinks(fixedClass.sClass, storage)));
+            }
+
+            return restoredData;
+        }
+
+        public static object RestoreLinks(ClassRoom data, EntityStorage storage)
+        {
+            List<ClassRoomType> roomTypes = new List<ClassRoomType>();
+            foreach (var roomType in data.ClassRoomTypes)
+            {
+                roomTypes.Add(storage.GetReference(roomType));
+            }
+            return new ClassRoom(((IDomainIdentity<ClassRoom>)data).ID, data.Number, data.Housing, roomTypes, data.SecondTypesMask);
+        }
+        public static object RestoreLinks(StudentsClass data, EntityStorage storage)
+        {
+            List<ClassRoomType> roomTypes = new List<ClassRoomType>();
+            foreach (var roomType in data.RequireForClassRoom)
+            {
+                roomTypes.Add(storage.GetReference(roomType));
+            }
+            List<StudentSubGroup> groups = new List<StudentSubGroup>();
+            foreach (var group in data.SubGroups)
+            {
+                groups.Add(storage.GetReference(group));
+            }
+            List<Teacher> teachers = new List<Teacher>();
+            foreach (var teacher in data.Teacher)
+            {
+                teachers.Add(storage.GetReference(teacher));
+            }
+            return new StudentsClass(((IDomainIdentity<StudentsClass>)data).ID, groups, teachers, data.Name, roomTypes);
+        }
         #endregion
     }
 }
